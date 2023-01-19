1c9933d9897664f30313e69a5717eacb09bdf851
Ahmed Tammam <tamam65@gmail.com>
2023-01-19 10:20:42 +0400

    intial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+
+  return (
+    <>
+      <Head>
+        <title>Inventory Management</title>
+        <meta name="description" content="Inventory Management App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h3>Inventory Management</h3>
+      </main>
+    </>
+  );
+};
+
+export default Home;
